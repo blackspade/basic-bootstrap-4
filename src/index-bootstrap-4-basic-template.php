bdf232c3145eda18cf2ae1569df21f86879e46bc
Bryant Ricart <craig.rootportal@gmail.com>
2023-01-18 12:23:13 -0500

    Add CSS Font Awesome 4
    Added CDN for jquery and font awesome 4

--- src/index-bootstrap-4-basic-template.php
+++ src/index-bootstrap-4-basic-template.php
@@ -1,23 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<meta charset="utf-8">
-<meta name="keywords" content="">
-<meta name="author" content="">
-<meta name="description" content=""/>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" >
-<title>Standard Template</title>
-<style>
-</style>
+	<head>
+	<meta charset="utf-8">
+	<meta name="keywords" content="">
+	<meta name="author" content="">
+	<meta name="description" content=""/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" >
+	<title>Standard Template</title>
+	<style>
+
+	</style>
 </head>
 <body>
 
 
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" ></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" ></script>
+<script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.3/js/jquery.tablesorter.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" ></script>
 <script>
+
 </script>
 </body>
 </html>
